e662ec33fb0ffbade48a9529c60bccdad706d4de
Joel Graycar <jgraycar@berkeley.edu>
2015-02-25 15:59:07 -0800

    Initializing rails app

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WorkshiftWebsite::Application.config.session_store :cookie_store, key: '_workshift-website_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WorkshiftWebsite::Application.config.session_store :active_record_store
