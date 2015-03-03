9eebc6608c7fc2c1cabbb21ccb5a977b75a1f5bc
Minh Daole <mdaole@berkeley.edu>
2015-03-02 17:04:05 -0800

    removed unneeded login_steps.rb

--- features/step_definitions/login_steps.rb
+++
@@ -1,9 +0,0 @@
-Given /^I am not signed in$/ do
-  current_driver = Capybara.current_driver
-  begin
-    Capybara.current_driver = :rack_test
-    page.driver.submit :delete, "/users/sign_out", {}
-  ensure
-    Capybara.current_driver = current_driver
-  end
-end
