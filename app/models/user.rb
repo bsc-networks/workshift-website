4796ce5db979c9f249b9080cfee402dca3dc2453
itzzshirlayyy <itzzshirlayyy@users.noreply.github.com>
2015-03-08 15:49:25 -0700

    Started process to send invitation to create account

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,8 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :invitable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :invitable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
 end
