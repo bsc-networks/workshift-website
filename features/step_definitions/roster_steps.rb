428cffaa5283bab43e5eeadd2f2a430d719e4667
Stephanie Kim <stephanie.kim@berkeley.edu>
2015-03-10 00:27:56 -0700

    removed sorted name feature

--- features/step_definitions/roster_steps.rb
+++ features/step_definitions/roster_steps.rb
@@ -4,11 +4,7 @@ Given /the following residents exist/ do |resident_table|
   end
 end
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  index1 = page.body.index(e1)
-  index2 = page.body.index(e2)
-  assert(index1 < index2, "not alphabetical")
-end
+
 
 Then /I should see all the residents/ do
   numResidents = page.all('#residents tr').size - 1
