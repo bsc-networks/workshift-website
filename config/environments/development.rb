d80ee3cc24e7f9dd372ecf867415dfae6cef2b1d
itzzshirlayyy <chan.m.trinh@gmail.com>
2015-03-10 12:45:19 -0700

    using letter opener gem for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,25 +14,13 @@ WorkshiftWebsite::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # care if the mailer can't send
+  # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :letter_opener
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
-  # Set up mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    domain: ENV['GMAIL_DOMAIN'],
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD']
-  }
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
