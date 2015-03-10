826d93ce5f91e6e79cb3d3190dee6e298c1ac330
Joel Graycar <jgraycar@berkeley.edu>
2015-03-10 02:52:27 -0700

    updating gitignore with config/local_env.yml

---
+++ features/step_definitions/roster_steps.rb
@@ -0,0 +1,12 @@
+Given /the following residents exist/ do |resident_table|
+  resident_table.hashes.each do |resident|
+    User.create(resident)
+  end
+end
+
+
+
+Then /I should see all the residents/ do
+  numResidents = page.all('#residents tr').size - 1
+  assert numResidents == 0
+end	
