1f77b358b599ad91c270eeeb9faadf85b4ad7e95
itzzshirlayyy <itzzshirlayyy@users.noreply.github.com>
2015-03-14 00:20:03 -0700

    added travis to app

--- spec/models/workshift_spec.rb
+++ spec/models/workshift_spec.rb
@@ -32,4 +32,37 @@ describe Workshift do
             ['Thursday', 4], ['Friday', 5], ['Saturday', 6]]
     expect(Workshift.valid_days).to match_array days
   end
+
+  it 'returns the right weekdays' do
+    expect(build(:workshift, day: 0).weekday).to eq("Sunday")
+    expect(build(:workshift, day: 1).weekday).to eq("Monday")
+    expect(build(:workshift, day: 2).weekday).to eq("Tuesday")
+    expect(build(:workshift, day: 3).weekday).to eq("Wednesday")
+    expect(build(:workshift, day: 4).weekday).to eq("Thursday")
+    expect(build(:workshift, day: 5).weekday).to eq("Friday")
+    expect(build(:workshift, day: 6).weekday).to eq("Saturday")
+    expect(build(:workshift, day: 7).weekday).to be_nil
+  end
+
+  it 'formats the times correctly' do
+    time1 = Time.new(2002, 10, 31, 2, 0, 0, "+02:00")
+    time2 = Time.new(2002, 10, 31, 2, 3, 0, "+02:00")
+    expect(build(:workshift, start_time: time1, end_time: time2).formatted_start_time).to eq(" 2:00 AM")
+    expect(build(:workshift, start_time: time1, end_time: time2).formatted_end_time).to eq(" 2:03 AM")
+  end
+
+  it 'returns the right number of users' do
+    workshift = build(:workshift, people_needed: 5)
+    workshift.users << build(:user)
+    workshift.users << build(:user)
+    expect(workshift.num_assigned).to eq(2)
+  end
+
+  it 'returns the right worker ratio' do
+    workshift = build(:workshift, people_needed: 5)
+    workshift.users << build(:user)
+    workshift.users << build(:user)
+    expect(workshift.assigned_so_far).to eq("2/5")
+  end
+
 end
