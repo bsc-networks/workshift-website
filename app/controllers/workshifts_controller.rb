aff8a92ee07190d20de15cafe9cdb84ca3236643
Joel Graycar <jgraycar@berkeley.edu>
2015-03-31 11:45:03 -0700

    Handled auth exception to flash error message

--- app/controllers/workshifts_controller.rb
+++ app/controllers/workshifts_controller.rb
@@ -52,4 +52,12 @@ class WorkshiftsController < ApplicationController
   def set_workshift
     @workshift = Workshift.find(params[:id])
   end
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    msg = 'You are not authorized to perform this action.'
+    flash[:alert] = I18n.t "pundit.#{policy_name}.#{exception.query}",
+                           default: msg
+    redirect_to request.referrer || root_url
+  end
 end
