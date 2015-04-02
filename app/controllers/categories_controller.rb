34ba677ed7e2297e70e15f86d9fe432a32e41747
ECIson <elliott-ison@berkeley.edu>
2015-04-02 00:27:31 -0300

    Category foundation created

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,83 +1,52 @@
 class CategoriesController < ApplicationController
-  # GET /categories
-  # GET /categories.json
+  before_filter :set_category, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
+
+  respond_to :html, :json
+
   def index
     @categories = Category.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @categories }
-    end
+    respond_with(@categories)
   end
 
-  # GET /categories/1
-  # GET /categories/1.json
   def show
-    @category = Category.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @category }
-    end
+    respond_with(@categorie)
   end
 
-  # GET /categories/new
-  # GET /categories/new.json
   def new
     @category = Category.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @category }
-    end
+    authorize @category
+    respond_with(@category)
   end
 
-  # GET /categories/1/edit
   def edit
-    @category = Category.find(params[:id])
+    #@category = Category.find(params[:id])
+    authorize @category
   end
 
-  # POST /categories
-  # POST /categories.json
   def create
     @category = Category.new(params[:category])
-
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render json: @category, status: :created, location: @category }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    authorize @category
+    if @category.save
+      respond_with(@category)
+    else
+      render :new
     end
   end
 
-  # PUT /categories/1
-  # PUT /categories/1.json
   def update
-    @category = Category.find(params[:id])
-
-    respond_to do |format|
-      if @category.update_attributes(params[:category])
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
+    authorize @category
+    @category.update_attributes(params[:category])
+    respond_with(@category)
   end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.json
   def destroy
-    @category = Category.find(params[:id])
+    authorize @category
     @category.destroy
+    respond_with(@category)
+  end
 
-    respond_to do |format|
-      format.html { redirect_to categories_url }
-      format.json { head :no_content }
-    end
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
