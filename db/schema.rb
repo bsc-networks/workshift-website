8081f7b98b70471a024b5bedaf5601f49bf89c4e
Joel Graycar <jgraycar@berkeley.edu>
2015-04-03 17:53:40 -0700

    removed category_id from workshifts_assignments

--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(:version => 20150402024213) do
     t.integer  "workshift_id"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
-    t.integer  "category_id"
   end
 
   create_table "workshifts", :force => true do |t|
