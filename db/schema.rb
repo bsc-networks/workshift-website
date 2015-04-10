c386b831c2a79cedf93fb3b86be86ad88259cd4b
Minh Daole <mdaole@berkeley.edu>
2015-04-09 22:59:22 -0700

    missing comma, fix merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(:version => 20150410045620) do
     t.integer  "workshift_id"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.integer  "category_id"
   end
 
   create_table "workshifts", :force => true do |t|
