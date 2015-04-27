c246c7aa21c081b16fa9d11413a1a17273f1950e
Minh Daole <mdaole@berkeley.edu>
2015-04-26 20:49:43 -0700

    fix broken step def

--- features/step_definitions/assignment_steps.rb
+++ features/step_definitions/assignment_steps.rb
@@ -1,7 +1,7 @@
 When (/^I assign "(.+)" to the workshift "(.+)"$/) do |resident, task|
   @user = User.find_by_name resident
   workshift = Workshift.find_by_task(task)
-  assignment = workshift.assign_worker(user.id)
+  assignment = workshift.assign_worker(@user.id)
 end
 
 When (/^I am assigned to the shift "([^"]+)"$/) do |shift|
