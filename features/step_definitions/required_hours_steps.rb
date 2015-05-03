ee2df387f93bfc3e86f04418c53bad89066c007b
itzzshirlayyy <itzzshirlayyy@users.noreply.github.com>
2015-05-03 11:56:47 -0700

    tests for user required hours

--- features/step_definitions/required_hours_steps.rb
+++ features/step_definitions/required_hours_steps.rb
@@ -2,6 +2,12 @@ When /^I change the new required hours to (.+)$/ do |hour|
 	fill_in 'New Required Hours', with: hour
 	click_button 'Change Required Hours'
 end
-Then /^I should see the Current Required Hours as 5.0$/ do
-	expect(page).to have_content("5.0")
+
+Then(/^I should see the Current Required Hours as (\d+)$/) do |arg1|
+  expect(page).to have_content(arg1)
 end
+
+Then(/^I should not see the Current Required Hours as (\d+)$/) do |arg1|
+  expect('Current Required Hours').to_not have_content(arg1)
+end
+
