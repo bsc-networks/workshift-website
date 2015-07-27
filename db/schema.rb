a83807d1f7395273a251cfa8ed8694f6ccb44c88
mlequeux <martin.lequeux.gruninger@gmail.com>
2015-07-27 03:31:50 -0700

    Added units table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150727101253) do
+ActiveRecord::Schema.define(:version => 20150727102630) do
 
   create_table "assigned_workshifts", :force => true do |t|
     t.integer  "workshift_id"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(:version => 20150727101253) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  create_table "units", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "units", ["name"], :name => "index_units_on_name", :unique => true
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
---
+++ spec/factories/units.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :unit do
+    
+  end
+
+end
