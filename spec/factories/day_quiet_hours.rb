2b7e25ceab3bfc41ceef1d8018b31a60df01ac51
Brian <brian.a.pugh@gmail.com>
2015-12-05 13:22:04 -0800

    tests for quiet hours feature

--- spec/factories/day_quiet_hours.rb
+++ spec/factories/day_quiet_hours.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :day_quiet_hour, :class => 'DayQuietHour' do
-    quiet_hours nil
   end
 
 end
--- spec/factories/quiet_hours.rb
+++ spec/factories/quiet_hours.rb
@@ -1,7 +1,5 @@
 FactoryGirl.define do
   factory :quiet_hour, :class => 'QuietHour' do
-    day_quiet_hours nil
-    quiet_hour_id nil
   end
 
 end
