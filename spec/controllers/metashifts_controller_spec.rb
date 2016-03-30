e093ba01b2b7df325f29df484d9611f25a16caec
momochanfitz <momochanfitz@gmail.com>
2016-03-30 17:51:54 +0000

    added all controller and model spec files

---
+++ spec/controllers/metashifts_controller_spec.rb
@@ -0,0 +1,4 @@
+# require 'rails_helper'
+
+# RSpec.describe MetashiftsController, type: :controller do
+# end
---
+++ spec/controllers/policies_controller_spec.rb
@@ -0,0 +1,4 @@
+# require 'rails_helper'
+
+# RSpec.describe PoliciesController, type: :controller do
+# end
