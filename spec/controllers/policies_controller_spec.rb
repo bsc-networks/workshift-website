220cb6bcd7a6f55c1065816a26c8c7f03a3cd49f
Giorgia Willits <gwillits@berkeley.edu>
2016-03-30 23:28:45 +0000

    policies controller spec

--- spec/controllers/policies_controller_spec.rb
+++ spec/controllers/policies_controller_spec.rb
@@ -1,4 +1,33 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe PoliciesController, type: :controller do
-# end
+RSpec.describe PoliciesController, type: :controller do
+    describe "creting a semester policy" do 
+        context "when I am an admin" do
+        end
+        context "when I am a member" do
+            it "should redirect me to the home page" do
+            end
+        end
+    end
+    describe "loading the semester policies page" do
+        context "when the policies for the semester have not been set" do
+            context "when I am an admin" do
+                it "should redirect me to the set semester policies page"
+            end
+            context "when I am not an admin" do
+                it "should redirect me to the policies have not been set page"
+            end
+        end
+        context "when the policies for the semester have been set" do
+            it "should "
+        end
+    end
+    describe "editing the semester policies" do
+        context "when I am an admin" do
+        end
+        context "when I am a member" do
+            it "should redirect me to the home page" do
+            end
+        end
+    end
+end
