c20ca73cff30aeda8b5083ad2e941db3cf5b153e
Giorgia Willits <gwillits@berkeley.edu>
2016-03-30 04:47:25 +0000

    db migrating...

--- db/migrate/20160330042742_add_balances_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddBalancesToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :hour_balance, :float, default: 0
-    add_column :users, :fine_balance, :float, default: 0
-  end
-end
