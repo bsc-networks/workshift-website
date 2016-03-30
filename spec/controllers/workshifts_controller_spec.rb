a0cb4216883f96efb66058a133270d5c3709a56e
momochanfitz <momochanfitz@gmail.com>
2016-03-30 15:02:47 +0000

    added back app controller spec

--- spec/controllers/workshifts_controller_spec.rb
+++ spec/controllers/workshifts_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe WorkshiftsController, type: :controller do
-end
+# RSpec.describe WorkshiftsController, type: :controller do
+# end
