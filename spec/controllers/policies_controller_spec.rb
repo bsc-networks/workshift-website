fe32e6d02e9a24313c24f0ee0ec87b2700a2d9c3
Giorgia Willits <gwillits@berkeley.edu>
2016-03-31 00:22:39 +0000

    policies rspec scaffold

--- spec/controllers/policies_controller_spec.rb
+++ spec/controllers/policies_controller_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PoliciesController, type: :controller do
-    describe "creting a semester policy" do 
+    describe "creating a semester policy" do 
         context "when I am an admin" do
             it "should select the New Policy template for rendering"
+            it "should initialize a policy object that is available to that template"
+            it "should save the policy"
+            it "should redirect to the view policy page"
         end
         context "when I am a member" do
             it "should redirect to the home page" do
@@ -12,10 +15,10 @@ RSpec.describe PoliciesController, type: :controller do
     end
     describe "loading the semester policies page" do
         context "when the policies for the semester have not been set" do
-            context "when I am an admin" do
+            context "and I am an admin" do
                 it "should redirect to the set semester policies page"
             end
-            context "when I am not an admin" do
+            context "and I am not an admin" do
                 it "should redirect to the policies have not been set page"
             end
         end
@@ -26,6 +29,10 @@ RSpec.describe PoliciesController, type: :controller do
     end
     describe "editing the semester policies" do
         context "when I am an admin" do
+            it "should select the Edit Policy template for rendering"
+            it "should make the correct policy available to that template"
+            it "should update the attributes of the policy"
+            it "should redirect to the view policy page"
         end
         context "when I am a member" do
             it "should redirect to the home page" do
