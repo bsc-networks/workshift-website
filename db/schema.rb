383f1ed45a0e673f6d799950525a7a13b724c6ee
Eric Nelson <ericmnelson.13@gmail.com>
2016-03-31 18:39:53 +0000

    Update code climatel.yml

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20160330230950) do
 
+  create_table "documents", force: :cascade do |t|
+    t.string   "filename"
+    t.string   "content_type"
+    t.binary   "file_contents"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
   create_table "metashifts", force: :cascade do |t|
     t.string   "category"
     t.string   "description"
