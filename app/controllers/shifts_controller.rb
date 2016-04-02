13913946125614ba25e1a3330a7a9e0ed7d53f53
momochanfitz <momochanfitz@gmail.com>
2016-04-02 05:58:48 +0000

    removed create

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -41,13 +41,13 @@ class ShiftsController < ApplicationController
   end
   
   def upload
-    if (not params[:file].blank?)
-      new_shifts = Metashift.import(params[:file])
-      @metashifts_uploaded = new_shifts
-    else
-      flash[:notice] = "No file specified."
-      redirect_to '/shifts/new'
-    end
+    # if (not params[:file].blank?)
+    #   new_shifts = Metashift.import(params[:file])
+    #   @metashifts_uploaded = new_shifts
+    # else
+    #   flash[:notice] = "No file specified."
+    #   redirect_to '/shifts/new'
+    # end
   end
 
   # GET /shifts/new
