ff61a6504874efa44ef9362eff1d6578591a6577
Eric Nelson <ericmnelson.13@gmail.com>
2016-04-04 01:35:38 +0000

    Few small changes in session controlling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin 
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue
+      session[:user_id] = nil
+      redirect_to login_path
+    end
   end
   helper_method :current_user
 
