e9a99fc3b31972c22b0ceca0c35a488abadabf31
Giorgia Willits <gwillits@berkeley.edu>
2016-04-05 07:33:30 +0000

    add date and completed to shifts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404215535) do
+ActiveRecord::Schema.define(version: 20160405071625) do
 
   create_table "metashifts", force: :cascade do |t|
     t.string   "category"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20160404215535) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "user_id"
+    t.datetime "date"
+    t.boolean  "completed"
   end
 
   add_index "shifts", ["metashift_id"], name: "index_shifts_on_metashift_id"
@@ -57,6 +59,7 @@ ActiveRecord::Schema.define(version: 20160404215535) do
     t.integer  "permissions"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+    t.string   "password"
     t.string   "password_digest"
     t.boolean  "sent_confirmation", default: false
     t.boolean  "has_confirmed",     default: false
