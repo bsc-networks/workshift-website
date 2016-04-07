48fcb3861b0442a89a5c446535e997d55d8a1eac
Eric Nelson <ericn@berkeley.edu>
2016-04-07 03:58:18 +0000

    Fixed my email

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 20160405075121) do
     t.integer  "permissions"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "password"
     t.string   "password_digest"
     t.boolean  "sent_confirmation",   default: false
     t.boolean  "has_confirmed",       default: false
